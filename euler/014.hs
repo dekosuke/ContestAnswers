2d61125d05c7535963eade217c1f18972738be71
JUN komiyama <deco.elb@gmail.com>
2011-10-19 00:31:37 +0900

    added euler/014.hs

---
+++ euler/014.hs
@@ -0,0 +1,16 @@
+import Data.List
+
+next x 
+  | even x = div x 2
+  | otherwise = 3 * x + 1 
+
+chain x = chain' x 0
+chain' x k  
+  | x==1 = (k+1)
+  | otherwise = chain' (next x) (k+1)
+
+mils = [1..999999]
+
+print2nd (a,b) = print b
+
+main = print2nd $ (head.reverse) (sort (map (\x->(chain x,x)) mils))
